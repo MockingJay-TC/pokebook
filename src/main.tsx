50f3b54a933e531737faa379c3a5b457a5dade2a
Victor Aremu <victor.aremu@peswa.finance>
2024-05-21 12:29:30 +0000

    remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
+import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@mantine/core/styles.css";
 import "./index.css";
-import { store } from "./store/store.ts";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
